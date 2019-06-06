e23a8661137b6d08def56fe8b0aae0724026552a
Martin M <mxa.code@gmail.com>
2019-06-06 15:22:35 +0100

    feat: useState - warm up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [name, setName] = useState('');
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <h3>{name}</h3>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
     </div>
   );
 };
